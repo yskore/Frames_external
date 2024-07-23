0b8c1634abb86061dcfc65d97885b5c623f629ab
Kore Ayoade <koa5@cornell.edu>
2024-07-23 19:46:42 +0100

    Add files via upload

---
+++ app/src/main/kotlin/com/example/frames/MainActivity.kt
@@ -0,0 +1,10 @@
+package com.example.frames
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
+//import com.xraph.plugin.flutter_unity_widget.OverrideUnityActivity
+
+//class MainActivity : OverrideUnityActivity() {
+//}
